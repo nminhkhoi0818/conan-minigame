bf9f34b9f50d75f1e8692eb0e8210c42c0b07049
nminhkhoi0818 <nminhkhoi0818@gmail.com>
2022-05-06 23:14:24 +0700

    Create question.js

---
+++ question.js
@@ -0,0 +1,28 @@
+//Array of Objects
+const quiz=[
+    {
+        q:'Conan nặng bao nhiêu kg ?',
+        options:['17','18','19','20'],
+        answer:1
+    },
+    {
+        q:'Bạn gái của trung sĩ Takagi là ai ?',
+        options:['Haibara','Sato','Ran','Yumi'],
+        answer:1
+    }, 
+    {
+        q:'Biệt danh "Thám tử ngủ gật" chỉ ai ?',
+        options:['Agasa','Kogoro Mori', 'Heiji Hattori', 'Wataru Takagi'],
+        answer:1
+    }, 
+    {
+        q:'Bí danh của Haibara Ai khi còn làm việc cho Tổ chức Áo đen là ?',
+        options:['Vermouth','Sherry','Shery', 'Vodka'],
+        answer:1
+    }, 
+    {
+        q:'Shinichi đã bị teo nhỏ do uống thuốc ?',
+        options:['APTX 1234','APTX 4869','APTX 4986','APTX 2314'],
+        answer:1
+    },  
+    ]
